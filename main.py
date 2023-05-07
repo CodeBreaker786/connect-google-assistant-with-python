b050df20237617da41700d20f4f124372ed0c5d8
Code Breaker <codebreakers786@gmail.com>
2023-05-08 04:18:39 +0500

    new changes

--- main.py
+++ main.py
@@ -1,51 +1,85 @@
-import sys
-import threading 
-import tkinter as tk
-import speech_recognition 
-import pyttsx3 as tts
+import os
+import openai
+from dotenv import load_dotenv
+import time
+import speech_recognition as sr
+import pyttsx3
+import numpy as np
+# from os.path import join, dirname
+# import matplotlib.pyplot as plt
+# ^ matplotlib is great for visualising data and for testing purposes but usually not needed for production
 
-class Assistant:
-    def _init__(self):
-      self.recognizer = speech_recognition.Recognizer()
-      self.speaker = tts.init()
-      self.speaker.setProperty("rate", 150)
-      self.root = tk.Tk()
-      self.label = tk.Label(text="Lisining")
-      self.label.pack()
-      threading.Thread(target=self.run_assistant()).start()
-      self.root.mainloop()
-      
-    def run_assistant(self):
-        while True:
-            try:
-                with speech_recognition.Microphone() as mic:
-                    self.recognizer.adjust_for_ambient_noise(mic,duration=0.2)
-                    audio = self.recognizer.listen(mic)
-                    text = self.recognizer.recognize_google(audio)
-                    text = text.Lower()  
-                    if "hey jake" in text:
-                        self.label.config(fg="red")
-                        audio = self.recognizer.listen(mic)
-                        text = self.recognizer.recognize_googLe(audio)
-                        text = text.Lower()
-                        if text == "stop":
-                            self.speaker.say("Bye")
-                            self.speaker.runAndWait()
-                            self.speaker.stop()
-                            self.root.destroy()
-                            sys.exit()
-                        else:
-                            if text is not None:
-                                response = self.assistant.request(text)
-                                if response is not None:
-                                    self.speaker.say(response)
-                                    self.speaker.runAndWait()
-                            self.label.config (fg="black")
-            except:
-                self.label.config(fg="black")
-                continue            
-            
-Assistant()           
-
-
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+model = 'gpt-3.5-turbo'
+# Set up the speech recognition and text-to-speech engines
+r = sr.Recognizer()
+engine = pyttsx3.init()
+voice = engine.getProperty('voices')[1]
+engine.setProperty('voice', voice.id)
+name = "YOUR NAME HERE"
+greetings = [f"whats up master {name}", 
+             "yeah?", 
+             "Well, hello there, Master of Puns and Jokes - how's it going today?",
+             f"Ahoy there, Captain {name}! How's the ship sailing?",
+             f"Bonjour, Monsieur {name}! Comment ça va? Wait, why the hell am I speaking French?" ]
  
+# Listen for the wake word "hey pos"
+def listen_for_wake_word(source):
+    r.adjust_for_ambient_noise(source)
+    print("Listening for 'Hey POS'...")
+
+    while True:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print(text)
+            if "hey pos" in text.lower():
+                print("Wake word detected.")
+                print(text)
+                engine.say(np.random.choice(greetings))
+                engine.runAndWait()
+                listen_and_respond(source)
+                break
+        except sr.UnknownValueError:
+            pass
+
+# Listen for input and respond with OpenAI API
+def listen_and_respond(source):
+    print("Listening...")
+
+    while True:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print(f"You said: {text}")
+            if not text:
+                continue
+
+            # Send input to OpenAI API
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}]) 
+            response_text = response.choices[0].message.content
+            print(f"OpenAI response: {response_text}")
+
+            # Speak the response
+            engine.say(response_text)
+            engine.runAndWait()
+
+            if not audio:
+                listen_for_wake_word(source)
+        except sr.UnknownValueError:
+            time.sleep(2)
+            print("Silence found, shutting up, listening...")
+            listen_for_wake_word(source)
+            break
+            
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+            engine.say(f"Could not request results; {e}")
+            engine.runAndWait()
+            listen_for_wake_word(source)
+            break
+
+# Use the default microphone as the audio source
+with sr.Microphone() as source:
+    listen_for_wake_word(source)
